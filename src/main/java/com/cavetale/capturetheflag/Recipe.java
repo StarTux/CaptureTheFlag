1455f29620dc7b42568d34bb5d4ae8ad6f7916a3
StarTux <ths.loy@gmail.com>
2025-05-03 22:22:57 +0200

    Fix item stack

--- src/main/java/com/cavetale/capturetheflag/Recipe.java
+++ src/main/java/com/cavetale/capturetheflag/Recipe.java
@@ -3,6 +3,7 @@ package com.cavetale.capturetheflag;
 import java.util.ArrayList;
 import java.util.List;
 import org.bukkit.inventory.ItemStack;
+import static com.cavetale.mytems.MytemsPlugin.mytemsPlugin;
 import static com.cavetale.mytems.util.Items.deserialize;
 import static com.cavetale.mytems.util.Items.serializeToBase64;
 
@@ -25,9 +26,19 @@ public final class Recipe {
 
     public List<ItemStack> getItems() {
         List<ItemStack> result = new ArrayList<>();
-        result.add(a != null ? deserialize(a) : null);
-        result.add(b != null ? deserialize(b) : null);
-        result.add(c != null ? deserialize(c) : null);
+        result.add(a != null ? deserialize2(a) : null);
+        result.add(b != null ? deserialize2(b) : null);
+        result.add(c != null ? deserialize2(c) : null);
         return result;
+    }
+
+    private ItemStack deserialize2(String in) {
+        if (in == null) return null;
+        final ItemStack result = deserialize(in);
+        if (result == null) return null;
+        final ItemStack result2 = mytemsPlugin().fixItemStack(result);
+        return result2 != null
+            ? result2
+            : result;
     }
 }
